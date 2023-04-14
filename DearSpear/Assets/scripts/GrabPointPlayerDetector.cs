5d7764edeeef88a97e05254a2a8b5ca6d6fb587b
GuillemTio <gtio@edu.tecnocampus.cat>
2023-04-14 18:49:56 +0200

    mekivukaaavaa

---
+++ DearSpear/Assets/scripts/GrabPointPlayerDetector.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class GrabPointPlayerDetector : MonoBehaviour
+{
+    [SerializeField]
+    float DetectionRange = 8;
+
+    Transform _player;
+
+    private bool isDetecting;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, DetectionRange);
+
+        Gizmos.color = Color.white;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsInRange())
+        {
+            isDetecting = true;
+            ChangeColor(isDetecting);
+        }
+        if (!IsInRange() && isDetecting)
+        {
+            isDetecting = false;
+            ChangeColor(isDetecting);
+        }
+    }
+
+    private void ChangeColor(bool detectorCheck)
+    {
+        if (detectorCheck)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+
+    private bool IsInRange()
+    {
+        return Vector2.Distance(_player.position, transform.position) < DetectionRange;
+    }
+}
