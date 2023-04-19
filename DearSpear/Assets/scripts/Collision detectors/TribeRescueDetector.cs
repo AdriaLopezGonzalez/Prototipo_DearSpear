250cda2982b1a45ac8a8e72bbcc64be4e766157b
GuillemTio <gtio@edu.tecnocampus.cat>
2023-04-20 00:19:31 +0200

    Los de la tribu

---
+++ DearSpear/Assets/scripts/Collision detectors/TribeRescueDetector.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class TribeRescueDetector : MonoBehaviour
+{
+    private Collider2D _collider;
+
+    private bool theyLeaving;
+
+    //private Transform tribe1;
+    //private Transform tribe2;
+    //private Transform tribe3;
+    private Transform bush;
+    private Transform[] tribesPeople;
+
+    public float tribeSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+        theyLeaving = false;
+
+        //tribe1 = transform.GetChild(0);
+        //tribe2 = transform.GetChild(1);
+        //tribe3 = transform.GetChild(2);
+        tribesPeople = new Transform[transform.childCount - 1];
+
+        for (int i = 0; i < transform.childCount - 1; i++)
+        {
+            tribesPeople[i] = transform.GetChild(i);
+        }
+
+        bush = transform.GetChild(transform.childCount - 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _collider.enabled = false;
+            theyLeaving = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (theyLeaving)
+        {
+            GetAway();
+        }
+    }
+
+    private void GetAway()
+    {
+        foreach (Transform tribe in tribesPeople)
+        {
+            if (tribe != null)
+            {
+
+                tribe.position -= new Vector3(tribeSpeed * Time.deltaTime, 0, 0);
+
+                if (tribe.position.x > bush.position.x - 0.25 && tribe.position.x < bush.position.x + 0.25)
+                {
+                    GameObject.Destroy(tribe.gameObject);
+                }
+
+            }
+        }
+    }
+}
