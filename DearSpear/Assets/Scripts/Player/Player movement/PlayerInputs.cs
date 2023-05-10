46addde0b1201627e10db04aed2339ee3bd3c0da
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2023-05-10 23:10:43 +0200

    a

--- DearSpear/Assets/Scripts/Player/Player movement/PlayerInputs.cs
+++ DearSpear/Assets/Scripts/Player/Player movement/PlayerInputs.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
+//[RequireComponent(typeof(PlayerInput))]
 public class PlayerInputs : MonoBehaviour
 {
 
@@ -28,6 +28,7 @@ public class PlayerInputs : MonoBehaviour
     private InputActionReference pointerPosition;
 
     private PlayerControls _playerControls;
+    [SerializeField]
     private PlayerInput _playerInput;
 
     public Vector2 AimSpearPosition;
@@ -36,8 +37,6 @@ public class PlayerInputs : MonoBehaviour
 
     [SerializeField]
     private Transform twistPoint;
-
-    public bool controlChanged;
     private void OnEnable()
     {
         _playerControls.Enable();
@@ -170,8 +169,6 @@ public class PlayerInputs : MonoBehaviour
     public void OnDeviceChange(PlayerInput pi)
     {
         usingController = pi.currentControlScheme.Equals("Controller") ? true : false;
-
-        controlChanged = true;
     }
 
     public void FreezePlayer()
