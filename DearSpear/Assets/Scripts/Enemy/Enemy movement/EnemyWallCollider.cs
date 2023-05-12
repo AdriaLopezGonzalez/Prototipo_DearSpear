af7bb69591e50f3a1a5f2e07cd9c77e18ccaa954
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2023-05-12 02:37:14 +0200

    a

--- DearSpear/Assets/Scripts/Enemy/Enemy movement/EnemyWallCollider.cs
+++ DearSpear/Assets/Scripts/Enemy/Enemy movement/EnemyWallCollider.cs
@@ -15,7 +15,7 @@ public class EnemyWallCollider : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground") /*|| collision.gameObject.CompareTag("Wall")*/)
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("BaseEnemy")/*|| collision.gameObject.CompareTag("Wall")*/)
         {
             //CanFlip?.Invoke();
             _patroling.CanFlip();
