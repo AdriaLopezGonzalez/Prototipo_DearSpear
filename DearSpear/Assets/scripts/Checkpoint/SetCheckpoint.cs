4afc6379b8436e71be71007226f9dcd375ad1290
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2023-05-31 18:18:07 +0200

    a

--- DearSpear/Assets/scripts/Checkpoint/SetCheckpoint.cs
+++ DearSpear/Assets/scripts/Checkpoint/SetCheckpoint.cs
@@ -6,11 +6,23 @@ public class SetCheckpoint : MonoBehaviour
 {
     public static Action<Transform> SetActiveCheckpoint;
 
+    private ParticleSystem activeParticles;
+
+    private void Start()
+    {
+        activeParticles = transform.GetComponentInChildren<ParticleSystem>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
             SetActiveCheckpoint?.Invoke(gameObject.transform);
+
+            if (!activeParticles.isPlaying)
+            {
+                activeParticles.Play();
+            }
         }
     }
 }
